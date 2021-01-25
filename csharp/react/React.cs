1483172742e8f8ec2863de6dcd37f080e4679e55
Briand Quentin <euphoriique@gmail.com>
2021-01-25 14:47:57 +0100

    React

---
+++ csharp/react/React.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Reactor
+{
+    public InputCell CreateInputCell(int value)
+    {
+        return new InputCell()
+        {
+            Value = value
+        };
+    }
+
+    public ComputeCell CreateComputeCell(IEnumerable<Cell> producers, Func<int[], int> compute)
+    {
+        compute();
+    }
+}
+
+public abstract class Cell
+{
+    public int Value;
+}
+
+public class InputCell : Cell
+{
+}
+
+public class ComputeCell : Cell
+{
+   
+    public EventHandler<int> Changed;
+}
